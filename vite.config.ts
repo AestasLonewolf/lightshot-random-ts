de5f43e6af5335eca30440493487787fcd0c80f4
Aaron <aestas.lonewolf@gmail.com>
2022-08-02 05:28:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "https://aestaslonewolf.github.io/lightshot-random-ts/",
     plugins: [react()],
     build: {
         outDir: "./docs",
